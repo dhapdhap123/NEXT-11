91623417fdfee5556ca228e26a6582a7f6596f4f
dhapdhap123 <dhapdhap123@naver.com>
2023-03-06 22:19:06 +0900

    0306

---
+++ introduction/color.js
@@ -0,0 +1,14 @@
+const color = [
+  "#3498db",
+  "#2ecc71",
+  "#9b59b6",
+  "#34495e",
+  "#f1c40f",
+  "#e67e22",
+  "#e74c3c",
+  "#ecf0f1",
+  "#95a5a6",
+];
+const whole = document.getElementsByClassName("whole_screen");
+whole.addEventListener("click", () => {});
+container;
