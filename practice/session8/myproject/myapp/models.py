3b7f67793ce4ff53e12e8541fb9b12e6d7cbdf7c
dhapdhap123 <dhapdhap123@naver.com>
2023-03-31 14:59:54 +0900

    session8_date

--- practice/session8/myproject/myapp/models.py
+++ practice/session8/myproject/myapp/models.py
@@ -5,6 +5,7 @@ class Article(models.Model):
     title = models.CharField(max_length = 200)
     category = models.CharField(null=True, max_length = 20)
     content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
